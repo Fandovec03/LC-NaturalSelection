243bc92db4de2435160a025971a8533c03babe26
blueman5 <147107803+blueman5@users.noreply.github.com>
2024-10-12 13:20:28 -0300

    Make the SandWorm "Smarter" and unpredictable.

--- ExperimentalEnemyInteractions/Patches/SandWormAIPatch.cs
+++ ExperimentalEnemyInteractions/Patches/SandWormAIPatch.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using HarmonyLib;
 using Unity.Mathematics;
@@ -14,17 +14,23 @@ namespace ExperimentalEnemyInteractions.Patches
         static float refreshCDtime = 0.4f;
         static EnemyAI? closestEnemy = null;
         static EnemyAI? targetEnemy = null;
+        static float emergeTimer = 0f;
+        static float maxEmergeTime = 20f;
+        static float minEmergeCooldown = 30f;
+        static float lastEmergeTime = -minEmergeCooldown;
 
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
         static void SandWormUpdatePatch(SandWormAI __instance)
         {
+            if (__instance.isEnemyDead) return;
+
             enemyList = EnemyAIPatch.GetOutsideEnemyList(__instance);
             closestEnemy = EnemyAIPatch.findClosestEnemy(enemyList, closestEnemy, __instance);
 
             if (Script.BoundingConfig.enableLeviathan.Value != true) return;
 
-            if (closestEnemy is ForestGiantAI || closestEnemy is BaboonBirdAI || closestEnemy is MouthDogAI || closestEnemy is BaboonBirdAI && closestEnemy is RadMechAI)
+            if (closestEnemy != null && (closestEnemy is ForestGiantAI || closestEnemy is BaboonBirdAI || closestEnemy is MouthDogAI || closestEnemy is RadMechAI))
             {
                 targetEnemy = closestEnemy;
             }
@@ -39,58 +45,80 @@ namespace ExperimentalEnemyInteractions.Patches
                 __instance.moveTowardsDestination = true;
                 __instance.movingTowardsTargetPlayer = false;
 
-                if (Vector3.Distance(targetEnemy.transform.position, __instance.transform.position) < 10f)
+                float distanceToTarget = Vector3.Distance(targetEnemy.transform.position, __instance.transform.position);
+                if (distanceToTarget < 15f && !__instance.emerged && !__instance.inEmergingState && Time.time - lastEmergeTime > minEmergeCooldown)
                 {
                     __instance.StartEmergeAnimation();
+                    lastEmergeTime = Time.time;
                 }
 
                 if (__instance.emerged)
                 {
-                    __instance.moveTowardsDestination = false;
-                    __instance.movingTowardsTargetPlayer = false;
+                    emergeTimer += Time.deltaTime;
+                    if (emergeTimer > maxEmergeTime || distanceToTarget > 20f)
+                    {
+                        __instance.SetInGround();
+                        emergeTimer = 0f;
+                    }
+                    else
+                    {
+                        __instance.SetDestinationToPosition(targetEnemy.transform.position, checkForPath: false);
+                        __instance.agent.speed = 8f;
+                    }
                 }
             }
+            else
+            {
+                if (!__instance.roamMap.inProgress && !__instance.emerged && !__instance.inEmergingState)
+                {
+                    __instance.StartSearch(__instance.transform.position, __instance.roamMap);
+                }
+                __instance.agent.speed = 4f;
+            }
         }
+
         [HarmonyPatch("DoAIInterval")]
         [HarmonyPostfix]
         static void SandWormDoAIIntervalPatch(SandWormAI __instance) 
         {
+            if (__instance.isEnemyDead) return;
+
             switch (__instance.currentBehaviourStateIndex)
             {
                 case 0:
                     if (!__instance.emerged && !__instance.inEmergingState)
                     {
-                        if (!__instance.roamMap.inProgress)
+                        if (UnityEngine.Random.value < 0.1f && Time.time - lastEmergeTime > minEmergeCooldown)
                         {
-                            __instance.StartSearch(__instance.transform.position, __instance.roamMap);
+                            __instance.StartEmergeAnimation();
+                            lastEmergeTime = Time.time;
                         }
-                    __instance.agent.speed = 4f;
-                    if (closestEnemy != null && Vector3.Distance(__instance.transform.position, closestEnemy.transform.position) < 15f)
-                    {
-                        __instance.SwitchToBehaviourState(1);
-                        __instance.chaseTimer = 0;
+                        else if (closestEnemy != null && Vector3.Distance(__instance.transform.position, closestEnemy.transform.position) < 20f)
+                        {
+                            __instance.SwitchToBehaviourState(1);
+                            __instance.chaseTimer = 0;
+                        }
                     }
-                }
-                break;
+                    break;
                 case 1:
                     if (__instance.roamMap.inProgress)
                     {
                         __instance.StopSearch(__instance.roamMap);
                     }
-                    targetEnemy = closestEnemy;
-                    if (Vector3.Distance(__instance.gameObject.transform.position, targetEnemy.transform.position) > 19f)
-                    {
-                        targetEnemy = null;
-                    }
-                    if (targetEnemy == null)
+                    if (targetEnemy == null || Vector3.Distance(__instance.gameObject.transform.position, targetEnemy.transform.position) > 25f)
                     {
                         __instance.SwitchToBehaviourState(0);
-                        break;
                     }
-                    __instance.SetDestinationToPosition(targetEnemy.transform.position, checkForPath: true);
-                    if (__instance.chaseTimer < 1.5f && Vector3.Distance(__instance.transform.position,targetEnemy.transform.position) < 4f &&  !(Vector3.Distance(StartOfRound.Instance.shipInnerRoomBounds.ClosestPoint(__instance.transform.position), __instance.transform.position) < 9f && UnityEngine.Random.Range(0,100) < 17))
+                    else
                     {
-                        __instance.StartEmergeAnimation();
+                        __instance.SetDestinationToPosition(targetEnemy.transform.position, checkForPath: !__instance.emerged);
+                        if (!__instance.emerged && !__instance.inEmergingState && __instance.chaseTimer < 1.5f && 
+                            Vector3.Distance(__instance.transform.position, targetEnemy.transform.position) < 10f &&
+                            Time.time - lastEmergeTime > minEmergeCooldown)
+                        {
+                            __instance.StartEmergeAnimation();
+                            lastEmergeTime = Time.time;
+                        }
                     }
                     break;
             }

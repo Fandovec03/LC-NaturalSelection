7e0f566a37b02f90d64b1c7e9ef06d2e1c195713
Fandovec03 <Fandovec03@gmail.com>
2025-05-10 01:04:28 +0200

    1

--- ExperimentalEnemyInteractions/EnemyPatches/BlobAIPatch.cs
+++ ExperimentalEnemyInteractions/EnemyPatches/BlobAIPatch.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using BepInEx.Logging;
 using GameNetcodeStuff;
 using HarmonyLib;
 using LethalNetworkAPI;
--- ExperimentalEnemyInteractions/EnemyPatches/EnemyAICollissionPatch.cs
+++ ExperimentalEnemyInteractions/EnemyPatches/EnemyAICollissionPatch.cs
@@ -1,3 +1,4 @@
+using BepInEx.Logging;
 using HarmonyLib;
 using NaturalSelection.Generics;
 using System;

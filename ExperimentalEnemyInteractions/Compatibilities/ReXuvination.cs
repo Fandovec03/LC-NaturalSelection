e498c896c81a3698811ea6b828c0ca58a15b2029
Fandovec03 <Fandovec03@gmail.com>
2025-07-01 21:55:56 +0200

    Update ReXuvination.cs

--- ExperimentalEnemyInteractions/Compatibilities/ReXuvination.cs
+++ ExperimentalEnemyInteractions/Compatibilities/ReXuvination.cs
@@ -40,11 +40,30 @@ namespace NaturalSelection.Compatibility
         }
 
         [HarmonyPatch(typeof(QuickMenuManagerPatch) ,"QuickMenuManagerStartPatch")]
-        [HarmonyPrefix]
-        static void QuickMenuManagerStartPatch()
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> QuickMenuManagerStartPatch(IEnumerable<CodeInstruction> instructions)
         {
-            QuickMenuManagerPatch.alreadyPatched = true;
+
+            foreach (var instruction in instructions)
+            {
+                Script.Logger.LogInfo(instruction.ToString());
+            }
+
+            CodeMatcher matcher = new CodeMatcher(instructions);
+
+            matcher.MatchForward(true,
+                new CodeMatch(OpCodes.Stloc_2),
+                new CodeMatch(OpCodes.Stloc_0,
+                new CodeMatch(OpCodes.Stloc_3)
+                ))
+                .ThrowIfInvalid("Failed to get a match.")
+                .Advance(1)
+                .InsertAndAdvance(new CodeInstruction(OpCodes.Ldloc_2))
+                .Insert(new CodeInstruction(OpCodes.Stloc_1));
+
             Script.Logger.LogMessage("Prevented Rexuvination from patching enemy colliders");
+
+            return matcher.Instructions();
         }
     }
 }
